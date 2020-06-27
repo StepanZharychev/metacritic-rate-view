85f3c061674b6405281de7b70b6abdbe5662c0f3
Stepan Zharychev <decadentsz@gmail.com>
2020-06-27 12:43:46 +0200

    Update Rate View with different charts.

--- web/index.js
+++ web/index.js
@@ -10,7 +10,22 @@ const transformKeysToColorPalette = keys => {
 
     return colors;
 };
+const getOldestReview = user => {
+    const { reviews } = user.userData;
 
+    if (reviews.length > 1) {
+        const sortedReviews = reviews.sort((a, b) => (new Date(a.date)).getTime() - (new Date(b.date)).getTime());
+        return sortedReviews[0];
+    }
+
+    return reviews[0];
+};
+
+const renderRateStats = (data, selector) => {
+    const averageRate = Math.round(data.reduce((total, entry) => total += entry.grade, 0) / data.length * 100) / 100;
+    const entryCount = data.length;
+    document.querySelector(selector).innerHTML = `<span class="rate-stat">${averageRate}</span> / <span class="count-stat">${entryCount} rates</span>`;
+}
 const renderRatesDistributionChart = (data, selector) => {
     let chartData = {};
     let totalNumberOfEntries = 0;
@@ -68,6 +83,58 @@ const renderRatesDistributionByEdgeValues = (data, selector) => {
         columns.push([fullKey, chartData[key]]);
         keys.push(fullKey);
     }
+
+    c3.generate({
+        bindto: selector,
+        data: {
+            columns,
+            type: 'pie',
+            colors: transformKeysToColorPalette(keys)
+        },
+        legend: {
+            position: 'right'
+        }
+    });
+};
+const renderProportionOfNewRates = (data, selector) => {
+    let stats = {
+        singleAccounts: {
+            count: 0,
+            rateSum: 0
+        },
+        newAccounts: {
+            count: 0,
+            rateSum: 0
+        },
+        oldAccounts: {
+            count: 0,
+            rateSum: 0
+        }
+    };
+
+    for (let entry of data) {
+        if (entry.userData.totalReviews <= 1 && entry.userData.totalRatings <= 1) {
+            stats.singleAccounts.count++;
+            stats.singleAccounts.rateSum += entry.grade;
+        } else {
+            const oldestRating = getOldestReview(entry);
+
+            if ((new Date(oldestRating.date)).getTime() < (new Date('Jun 19, 2020')).getTime()) {
+                stats.oldAccounts.count++;
+                stats.oldAccounts.rateSum += entry.grade;
+            } else {
+                stats.newAccounts.count++;
+                stats.newAccounts.rateSum += entry.grade;
+            }
+        }
+    }
+
+    const singleAverageRate = Math.round(stats.singleAccounts.rateSum / stats.singleAccounts.count * 100) / 100;
+    const newAverageRate = Math.round(stats.newAccounts.rateSum / stats.newAccounts.count * 100) / 100;
+    const oldAverageRate = Math.round(stats.oldAccounts.rateSum / stats.oldAccounts.count * 100) / 100;
+
+    const keys = [`Single Voters (1 rating/review at all)`, `New Voters (more than 1 rating, but after release)`, `Old Voters (more than 1 rating, including before release)`];
+    let columns = [[keys[0], stats.singleAccounts.count], [keys[1], stats.newAccounts.count], [keys[2], stats.oldAccounts.count]];
 
     c3.generate({
         bindto: selector,
@@ -158,10 +225,12 @@ window.onload = async () => {
     let data = await fetch('/data/index.json');
     data = await data.json();
 
+    renderRateStats(data, '#stat1');
     renderRatesDistributionChart(data, '#chart1');
     renderRatesDistributionChart(data.filter(entry => entry.date === 'Jun 19, 2020'), '#chart2');
     renderRatesDistributionByEdgeValues(data, '#chart3');
     renderRatesDistributionByEdgeValues(data.filter(entry => entry.date === 'Jun 19, 2020'), '#chart4');
-    renderApprovalRateDistribution(data.filter(entry => entry.approvalRate), '#chart5');
-    renderStatisticsByKeywords(data, ['garbage', 'Druckmann', 'SJW', 'gay', 'lesbian', 'gameplay', 'masterpiece'], '#chart6');
+    renderProportionOfNewRates(data, '#chart5')
+    renderApprovalRateDistribution(data.filter(entry => entry.approvalRate), '#chart6');
+    renderStatisticsByKeywords(data, ['garbage', 'Druckmann', 'SJW', 'gay', 'lesbian', 'gameplay', 'masterpiece'], '#chart7');
 };
